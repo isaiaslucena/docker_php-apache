0d1fa3358347b0ad14d3933ba15cbe8c2aa4564b
Isaias Neto <isaiasneto@gmail.com>
2020-02-14 22:12:27 -0300

    add xsend to app directory

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 if [[ -f "/FIRST_RUN" ]] ; then
 	rm -rf /FIRST_RUN
+	sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+	echo "<Directory /var/www/html>" >> /etc/apache2/apache2.conf
+	echo "    XSendFilePath /var/www/html " >> /etc/apache2/apache2.conf
+	echo "    XSendFile on" >> /etc/apache2/apache2.conf
+	echo "</Directory>" >> /etc/apache2/apache2.conf
 	a2enmod rewrite
 	composer install
 fi
