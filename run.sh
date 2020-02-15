0391da2115d6c2a7082c4a2b6769b21e3b135aea
Isaias Neto <isaiasneto@gmail.com>
2020-02-14 22:57:00 -0300

    Changed echo inside apache conf

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@
 if [[ -f "/FIRST_RUN" ]] ; then
 	rm -rf /FIRST_RUN
 	sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-	echo "<Directory /var/www/html>" >> /etc/apache2/apache2.conf
-	echo "    XSendFilePath /var/www/html " >> /etc/apache2/apache2.conf
-	echo "    XSendFile on" >> /etc/apache2/apache2.conf
-	echo "</Directory>" >> /etc/apache2/apache2.conf
+	echo "" >> /etc/apache2/apache2.conf.
+	echo "XSendFile on" >> /etc/apache2/apache2.conf
+	echo "XSendFilePath /disks" >> /etc/apache2/apache2.conf
 	a2enmod rewrite
 	composer install
 fi
